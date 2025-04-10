1ba5132b5246810385d04d7128a56553c1e5dfd2
Vinayak2k03 <vinayaknagar2810@gmail.com>
2025-04-10 19:23:23 +0530

    fix build issues

---
+++ packages/db/src/prisma.d.ts
@@ -0,0 +1,3 @@
+import { PrismaClient } from "@prisma/client";
+export declare const prismaClient: PrismaClient<import("prisma-client-b818b3d707ad9235f8ae162ade77fbb06259a69947c94aeac8a0354ec5e2a9b0").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
+//# sourceMappingURL=prisma.d.ts.map
